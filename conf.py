75ddfb576ac3a9c291f1b83a9b7db65f0b8fdc15
sophiesam221 <sophiesam221@protonmail.com>
2026-02-27 21:58:19 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,7 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
-html_title = "Trend Micro Download Already Purchased – The Ultimate Device-Ready Installation Plan"
+html_title = "Steps to Install CCleaner Professional on a New Computer: Download and Activate with License Key Easily"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
